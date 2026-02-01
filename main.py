486f23d8982dcba36b5180173c2c011f4f2f73d9
mdmotaleb-2024413266-pixel <mdmotaleb-2024413266@eee.du.ac.bd>
2026-02-01 11:53:10 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st, time
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+st.set_page_config(page_title="LAGmate", layout="wide")
+if "messages" not in st.session_state:
+    st.session_state.messages = [SystemMessage(content="You are a helpful assistant and your name is LAGmate")]
+
+st.title(":orange[LAGmate]")
+st.divider()
+with st.sidebar:
+    st.header(":green[Mohammad Miraz Ali]")
+    st.write(":green[Dept. of EEE]\n\n:green[University of Dhaka]")
+
+model = ChatGroq(model="llama-3.3-70b-versatile", groq_api_key="GROQ_API_KEY")
+
+for m in st.session_state.messages:
+    if not isinstance(m, SystemMessage):
+        with st.chat_message("user" if isinstance(m, HumanMessage) else "assistant"):
+            st.markdown(m.content)
+
+if user_input := st.chat_input("Say...something ??"):
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    st.chat_message("user").markdown(user_input)
+    
+    with st.chat_message("assistant"):
+        asst_box, full_reply = st.empty(), ""
+        try:
+            response = model.invoke(st.session_state.messages).content
+            for char in response:
+                full_reply += char
+                asst_box.markdown(full_reply + "▌")
+                time.sleep(0.005)
+            asst_box.markdown(response)
+            st.session_state.messages.append(AIMessage(content=response))
+        except Exception as e:
+            st.error(f"Error: {e}")
